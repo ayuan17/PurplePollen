900b3987c8c2716fa8f693f55da7b3779037eb2b
Alex Yuan <alexyuan@lawn-128-61-17-219.lawn.gatech.edu>
2017-03-16 20:35:43 -0400

    test

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,4 +1,4 @@
-var queryURL = "http://food2fork.com/api/search?key=1f35aa9b21774c9228255e0d0e0844f2&q=shredded%20chicken";
+var queryURL = "https://food2fork.com/api/search?key=1f35aa9b21774c9228255e0d0e0844f2&q=shredded%20chicken";
 
    $.ajax({
      url: queryURL,
