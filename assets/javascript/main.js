c4146dc16fa5f7636349e1aaf95412fc5512a725
Alex Yuan <alexyuan@lawn-128-61-17-219.lawn.gatech.edu>
2017-03-16 21:12:45 -0400

    removes s on queryUrl

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,8 +1,9 @@
-var queryURL = "https://food2fork.com/api/search?key=1f35aa9b21774c9228255e0d0e0844f2&q=shredded%20chicken";
+var queryURL = "http://food2fork.com/api/search?key=1f35aa9b21774c9228255e0d0e0844f2&q=shredded%20chicken";
 
    $.ajax({
      url: queryURL,
      method: 'GET'
    }).done(function(response) {
-     console.log(response);
+     console.log(response.title);
    });
+
