194e373fb008e25d4e793e75c45f51546f035701
Alex Yuan <alexyuan@lawn-128-61-30-161.lawn.gatech.edu>
2017-03-18 11:03:44 -0400

    some updates to html

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,9 +1,15 @@
-var queryURL = "http://food2fork.com/api/search?key=1f35aa9b21774c9228255e0d0e0844f2&q=shredded%20chicken";
+var queryURL = "http://food2fork.com/api/search?key=ff84bc9ded705856321486ca38126b53&q=shredded%20chicken";
 
    $.ajax({
      url: queryURL,
-     method: 'GET'
+     method: 'GET',
+     crossDomain: true,
+     dataType: 'json',
+     beforeSend: function(request) {
+        request.setRequestHeader('Access-Control-Allow-Origin', 'x-request-head');
+        console.log(request);
+      },
    }).done(function(response) {
-     console.log(response.title);
+     console.log(response);
    });
 
